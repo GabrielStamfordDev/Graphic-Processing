27d917c8a1040b2b51be7f2ac9eaa632978605bf
samuellh12 <samuellh12cpc@gmail.com>
2026-04-03 12:03:35 -0300

    Add SceneParser and related

---
+++ src/Scene/useExample.cpp
@@ -0,0 +1,18 @@
+#include "sceneParser.cpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    try {
+        Scene scene = SceneJsonLoader::loadFile("sampleScene.json");
+        
+        cerr << "Loaded " << scene.objects.size() << " objects\n";
+        cerr << "Loaded " << scene.lightList.size() << " lights\n";
+        cerr << "Camera image: " << scene.camera.image_width << " x " << scene.camera.image_height << "\n";
+
+        // cout << scene << endl;
+    } catch (const exception& e) {
+        cerr << "Scene load error: " << e.what() << "\n";
+        return 1;
+    }
+}
